62694e1c204a860a2f2c27ce1b22b9cad885d2ee
Kyle Windsor <kyle@kylewindsor.io>
2022-02-10 01:57:39 -0500

    fix serverside typescript, fix clientside sorting of new comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import * as express from 'express';
-import * as cors from 'cors';
+import express from 'express';
+import cors from 'cors';
 
 import { Init } from './lib/db';
 import CommentsRouter from './routes/comments';
